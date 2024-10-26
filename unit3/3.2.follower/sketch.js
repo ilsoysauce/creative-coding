2e8f9c6d95d478c36790ce4f3c664086445df492
ilsoysauce <ilseacf4@gmail.com>
2024-10-25 22:18:56 -0400

    follower

--- unit3/3.2.follower/sketch.js
+++ unit3/3.2.follower/sketch.js
@@ -13,9 +13,11 @@ let squarey=400;
 
 let img;
 let img2;
-function preload(){
+let back;
+function preload(){ //https://p5js.org/reference/p5/preload/
  img=loadImage("https://i.imgur.com/TlnlagT.png");
-img2=loadImage("https://cdn.pixabay.com/photo/2024/04/05/09/01/ai-generated-8676957_1280.png")
+img2=loadImage("https://cdn.pixabay.com/photo/2024/04/05/09/01/ai-generated-8676957_1280.png");
+back=loadImage("https://static.vecteezy.com/system/resources/thumbnails/008/459/776/small/green-natural-organic-grass-background-and-texture-vector.jpg");
 }
 
 
@@ -26,20 +28,8 @@ function setup() {
 }
 
 function draw() {
-  background(220);
-if (debug){
-   text("width: " + width, 30, 30);
-   text("height: " + height, 30, 45);
-   text("mouseX: " + mouseX, 30, 60);
-   text("mouseY: " + mouseY, 30, 75);
-   
-}
-// points text, push/pop so it doesnt affect the debug text
-push();
-textSize(50);
-textAlign(CENTER);
-text("points: " + points, width/2, 45);
-pop();
+  background(back);
+
 
 //code from the notes
 if (mouseX > x){
@@ -79,5 +69,22 @@ if(dist(squarex, squarey, mouseX, mouseY) < 50) //moves the square if the mouse 
 x = x+xspeed; //iterate x
 y = y+yspeed; //iterate y
 image(img,x-30,y-30,60,60);
+
+
+
+//moved the text to the bottom so that the objects wouldnt cover them
+if (debug){
+   text("width: " + width, 30, 30);
+   text("height: " + height, 30, 45);
+   text("mouseX: " + mouseX, 30, 60);
+   text("mouseY: " + mouseY, 30, 75);
+   
+}
+// points text, push/pop so it doesnt affect the debug text
+push();
+textSize(50);
+textAlign(CENTER);
+text("Points: " + points, width/2, 45);
+pop();
 
 }
